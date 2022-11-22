14a3c32662c444673c196d61e523f6c3359c3148
SametDEMR <115659092+SametDEMR@users.noreply.github.com>
2022-11-22 11:42:15 +0300

    SAYI TAHMİN OYUNU

--- main.c
+++ main.c
@@ -1,68 +1,237 @@
+//SAYI TAHMÝN OYUNU
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <math.h>
+#include <time.h>
 
-#define Maksimum(s1,s2) (s1>s2) ? s1 : s2
-#define Minimum(s1,s2) (s2>s1) ? s1 : s2
+
 
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"Turkish");
 	
-	int a, b, c, d, buyuk, f, kucuk, devam;
+	int islem, hak, sayi, tahmin, oyuna;
 	
-	printf("<---ÜÇGEN EÞÝTSÝZLÝÐÝ--->\n");
+	anatekrar:
+	
+	printf("\t*****SAYI TAHMÝN OYUNU*****\n\n");
+	
 	tekrar:
-	printf("ÜÇGENÝN 1.KENARINI GÝRÝNÝZ --> ");
-	scanf("%d", &a);
-	printf("\n");
-	
-	printf("ÜÇGENÝN 2. KENARINI GÝRÝNÝZ --> ");
-	scanf("%d", &b);
-	printf("\n");
-	
-	printf("ÜÇGENÝN 3. KENARINI GÝRÝNÝZ --> ");
-	scanf("%d", &c);
-	printf("\n");
-	
-	d = Minimum(a, b);
-	kucuk = Minimum(c, d);
-	
-	f = Maksimum(a, b);
-	buyuk = Maksimum(c, d);
-	
+	printf("ZORLUK MODUNU SEÇÝNÝZ=\n\n");
+	printf("1-->\tKOLAY\n");
+	printf("2-->\tORTA\n");
+	printf("3-->\tZOR\n");
+	printf("--->");
+	scanf("%d", &islem);
 	printf("\n\n");
-	
-	if(f + kucuk > buyuk)
+	switch(islem)
 	{
-		if(buyuk > f - kucuk)
+		case 1 : goto kolay; break;
+		
+		case 2 : goto orta; break;
+		
+		case 3 : goto zor; break;
+		
+		default : printf("YANLIÞ SEÇÝM YAPTINIZ, TEKRAR DENEYÝNÝZ.\n\n"); goto tekrar; break;
+	}
+	
+	
+	kolay:
+		printf("SAYILAR 1-10[DAHÝL] ARASINDADIR.\n\n\n");
+		hak = 3;
+		srand(time(NULL));
+		srand(time(NULL)); 
+		sayi = rand()%10 + 1;
+		kalan1:
+		printf("KALAN HAKKINIZ\t%d\n\n", hak);
+		
+		if(hak == 0)
 		{
-			printf("\tGÝRÝLEN DEÐERLER BÝR ÜÇGEN OLUÞTURABÝLÝR.");
-			goto devametmek;
+			goto kaybet;
 		}
 		else
 		{
-			printf("\tGÝRÝLEN DEÐERLER BÝR ÜÇGEN OLUÞTURAMAZ.");
-			goto devametmek;
+			yanlis1:
+			printf("TAHMÝNÝNÝZÝ GÝRÝNÝZ-->");
+			scanf("%d", &tahmin);
+			printf("\n");
+			if(tahmin < 1 || tahmin > 10)
+			{
+				printf("ARALIK DIÞI DEÐER GÝRDÝNÝZ.TEKRAR DENEYÝNÝZ.\n\n");
+				goto yanlis1;
+			}
+			else
+			{
+				if(tahmin == sayi)
+				{
+					goto kazan;
+				}
+				else
+				{
+					if(sayi > tahmin)
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN KÜÇÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan1;
+					}
+					else
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN BÜYÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan1;
+					}
+				}
+			}
 		}
-	}
-	else
-	{
-		printf("\tGÝRÝLEN DEÐERLER BÝR ÜÇGEN OLUÞTURAMAZ.");
-		goto devametmek;
-	}
+			
+		
+		
+		return 0;
 	
-	devametmek:
-	printf("\n\n\t\tDEVAM ETMEK ÝSTER MÝSÝNÝZ?\n1--->EVET\n2--->HAYIR\n----> ");
-	scanf("%d", &devam);
-	if(devam == 1)
-	{
-		goto tekrar;
-	}
-	else
-	{
-		printf("\n\n\t<---UYGULAMAMI KULLANDIÐINIZ ÝÇÝN TEÞEKKÜR EDERÝM--->");
-	}
+	orta:
+		printf("SAYILAR 1-20[DAHÝL] ARASINDADIR.\n\n\n");
+		hak=5;
+		srand(time(NULL));
+		srand(time(NULL)); 
+		sayi = rand() % 20 + 1;
+		kalan2:
+		printf("KALAN HAKKINIZ\t%d\n\n",hak);
+		
+		if(hak == 0)
+		{
+			goto kaybet;
+		}
+		else
+		{
+			yanlis2:
+			printf("TAHMÝNÝNÝZÝ GÝRÝNÝZ-->");
+			scanf("%d", &tahmin);
+			printf("\n");
+			
+			if(tahmin < 1 || tahmin > 20)
+			{
+				printf("ARALIK DIÞI DEÐER GÝRDÝNÝZ.TEKRAR DENEYÝNÝZ.\n\n");
+				goto yanlis2;
+			}
+			else
+			{
+				if(tahmin == sayi)
+				{
+					goto kazan;
+				}
+				else
+				{
+					if(sayi > tahmin)
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN KÜÇÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan2;
+					}
+					else
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN BÜYÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan2;
+					}
+				}
+			}
+		}
+		
+		return 0;
+	
+	zor:
+		printf("SAYILAR 1-30[DAHÝL] ARASINDADIR.\n\n\n");
+		hak=7;
+		srand(time(NULL));
+		srand(time(NULL)); 
+		sayi = rand() % 30 + 1;
+		kalan3:
+		printf("KALAN HAKKINIZ\t%d\n\n",hak);
+		
+		if(hak == 0)
+		{
+			goto kaybet;
+		}
+		else
+		{
+			if(tahmin < 1 || tahmin > 30)
+			{
+				printf("ARALIK DIÞI DEÐER GÝRDÝNÝZ.TEKRAR DENEYÝNÝZ.\n\n");
+				goto yanlis2;
+			}
+			else
+			{
+				if(tahmin == sayi)
+				{
+					goto kazan;
+				}
+				else
+				{
+					if(sayi > tahmin)
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN KÜÇÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan3;
+					}
+					else
+					{
+						printf("TAHMÝNÝNÝZ SAYIDAN BÜYÜKTÜR.\n");
+						printf("TEKRAR DENEYÝNÝZ.\n\n");
+						hak--;
+						goto kalan3;
+					}
+				}
+			}
+		}
+		
+		return 0;
+	
+	kazan:
+		printf("<---\tOYUNU KAZANDINIZ TEBRÝKLER.\t--->\n");
+		printf("TEKRAR OYNAMAK ÝSTER MÝSÝNÝZ?\n\n\n");
+		printf("1-->EVET\n2-->HAYIR\n");
+		scanf("%d", &oyuna);
+		
+		if(oyuna == 1)
+		{
+			goto anatekrar;
+		}
+		else
+		{
+			goto bitis;
+		}
+	
+	kaybet:
+		printf("<---\tKAYBETTÝNÝZ.\t--->\n");
+		printf("BÝLGÝSAYARIN TUTTUÐU SAYI= %d\n\n\n", sayi);
+		printf("TEKRAR OYNAMAK ÝSTER MÝSÝNÝZ?\n");
+		printf("1-->EVET\n2-->HAYIR\n");
+		scanf("%d", &oyuna);
+		
+		if(oyuna == 1)
+		{
+			printf("\n");
+			goto anatekrar;
+		}
+		else
+		{
+			printf("\n");
+			goto bitis;
+		}
+	
+	
+	
+	
+	
+	bitis:
+		printf("<---\tOYUNU OYNADIÐINIZ ÝÇÝN TEÞEKKÜRLER.\t--->"); return 0;
 	
 	getchar();
 	return 0;
